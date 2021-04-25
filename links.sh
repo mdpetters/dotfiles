d75e0fc28ce458b9dccad5fa7c4daa2b9960021c
Markus Petters <mdpetter@ncsu.edu>
2021-04-25 15:18:17 -0400

    add alacritty config

--- links.sh
+++ links.sh
@@ -11,3 +11,4 @@ ln -s $CURRENT/.tmux.conf $HOME/.tmux.conf
 ln -s $CURRENT/.tmux-themepack $HOME/.tmux-themepack
 ln -s $CURRENT/.tmux $HOME/.tmux
 ln -s $CURRENT/vifm $HOME/.config/
+ln -s $CURRENT/alacritty $HOME/.config/
